dd099b0cf7c99da508120a17f3e1dc4ebf02ecbe
matcool <26722564+matcool@users.noreply.github.com>
2021-05-11 14:38:57 -0300

    shut up typescript!!

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -16,7 +16,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType, computed, ref } from 'vue';
+import { defineComponent } from 'vue';
 
 export default defineComponent({
     name: 'Modal',
